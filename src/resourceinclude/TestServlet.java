0e48d1695395fab9313f08309f403a8863c6759c
Arjan Tijms <arjan.tijms@gmail.com>
2012-02-21 23:48:08 +0100

    Initial commit - very basic setup

---
+++ src/resourceinclude/TestServlet.java
@@ -0,0 +1,20 @@
+package resourceinclude;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/testservlet")
+public class TestServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+    
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+       response.getWriter().write("Output from Servlet");       
+    }
+
+}
