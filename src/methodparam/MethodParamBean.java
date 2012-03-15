ac5d868706619f1827ac71003037f07c9f86ed74
Arjan Tijms <arjan.tijms@gmail.com>
2012-03-16 00:11:36 +0100

    Added demo/test for methodParam handler.

---
+++ src/methodparam/MethodParamBean.java
@@ -0,0 +1,49 @@
+package methodparam;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.event.ActionEvent;
+
+@ManagedBean
+public class MethodParamBean {
+
+	private boolean actionInvoked;
+	private boolean listenerInvoked;
+	private boolean listenerWithEventInvoked;
+
+	public void doAction() {
+		actionInvoked = true;
+	}
+	
+	public void doActionWithParam(String dummy) {
+		actionInvoked = true;
+	}
+	
+	public void actionListener() {
+		listenerInvoked = true;
+	}
+	
+	public void actionListenerWithParam(String dummy) {
+		listenerInvoked = true;
+	}
+	
+	public void actionListenerWithClientParam(ActionEvent event) {
+		listenerWithEventInvoked = true;
+	}
+	
+	public MethodParamBean getBean() {
+		return this;
+	}
+	
+	public boolean getActionInvoked() {
+		return actionInvoked;
+	}
+	
+	public boolean getListenerInvoked() {
+		return listenerInvoked;
+	}
+	
+	public boolean isListenerWithEventInvoked() {
+		return listenerWithEventInvoked;
+	}
+	
+}
