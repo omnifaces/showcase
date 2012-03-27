aa9bfbee79c63bd66294c9a572cee89c5ceb2895
Bauke Scholtz <balusc@gmail.com>
2012-03-27 13:01:15 -0400

    Added basic layout template with GA tracking (and turned nbsp into #160 and used command buttons instead of command links on outputlabel demo, removed superfluous attribute name from @WebServlet)

--- src/resourceinclude/TestServlet.java
+++ src/resourceinclude/TestServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = "/testservlet")
+@WebServlet("/testservlet")
 public class TestServlet extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
