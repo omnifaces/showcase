11bcda3f6a9dbe310eafd528d3d2d48471ca9002
Bauke Scholtz <balusc@gmail.com>
2012-03-28 17:03:02 -0400

    Added FNFE to FacesExceptionFilter to handle 404s properly

--- src/org/omnifaces/showcase/FacesExceptionFilter.java
+++ src/org/omnifaces/showcase/FacesExceptionFilter.java
@@ -1,5 +1,6 @@
 package org.omnifaces.showcase;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import javax.faces.FacesException;
@@ -10,15 +11,19 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
- * JSF wraps all business exceptions in a {@link FacesException}. This filter will extract its cause until it's not a
- * {@link FacesException} anymore and then rethrow it in a {@link ServletException} so that the standard Servlet API
- * error page handling can do its job properly on business exceptions thrown during JSF requests.
+ * This filter will solve 2 problems with faces exceptions.
+ * <ol>
+ * <li>Mojarra's FacesFileNotFoundException needs to be interpreted as 404.
+ * <li>Root cause of FacesException needs to be unwrapped to utilize standard Servlet API error page handling.
+ * </ol>
  *
  * @author Bauke Scholtz
  */
-@WebFilter(servletNames = "facesServlet")
+@WebFilter(servletNames = { "facesServlet", "FacesServlet", "Faces Servlet" })
 public class FacesExceptionFilter implements Filter {
 
 	@Override
@@ -27,15 +32,18 @@ public class FacesExceptionFilter implements Filter {
 	}
 
 	@Override
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
 		try {
 			chain.doFilter(request, response);
+		}
+		catch (FileNotFoundException e) {
+			((HttpServletResponse) response).sendError(HttpServletResponse.SC_NOT_FOUND, ((HttpServletRequest) request).getRequestURI());
 		}
 		catch (ServletException e) {
 			Throwable cause = e.getCause();
 
 			while (cause instanceof FacesException && cause.getCause() != null) {
-				 cause = cause.getCause();
+				cause = cause.getCause();
 			}
 
 			throw new ServletException(cause);
