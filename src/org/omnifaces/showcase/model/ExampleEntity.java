f040745634d5b56daa2e037819b9d1e0ddd604b3
Bauke Scholtz <balusc@gmail.com>
2012-05-03 18:06:24 -0400

    Fixed/improved some stuff after testing with MyFaces

--- src/org/omnifaces/showcase/model/ExampleEntity.java
+++ src/org/omnifaces/showcase/model/ExampleEntity.java
@@ -1,6 +1,10 @@
 package org.omnifaces.showcase.model;
 
-public class ExampleEntity {
+import java.io.Serializable;
+
+public class ExampleEntity implements Serializable {
+
+	private static final long serialVersionUID = 1L;
 
 	private Long id;
 	private String value;
