04cf9824ed7404d81235a34430da1537aea63ba0
Arjan Tijms <arjan.tijms@gmail.com>
2012-06-01 23:22:29 +0200

    Added FacesViews demo pages.

---
+++ src/org/omnifaces/showcase/facesviews/PostRedirectBean.java
@@ -0,0 +1,18 @@
+package org.omnifaces.showcase.facesviews;
+
+import javax.enterprise.context.RequestScoped;
+import javax.faces.bean.ManagedBean;
+
+@RequestScoped
+@ManagedBean
+public class PostRedirectBean {
+    
+    public String toViewsDemo() {
+        return "/viewsdemo?faces-redirect=true";
+    }
+
+    public String toSecond() {
+        return "/other/second?faces-redirect=true";
+    }
+    
+}
