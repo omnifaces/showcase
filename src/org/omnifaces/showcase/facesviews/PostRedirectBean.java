a6ae9624bc6282d476cccfd76f4b34c8cabfc676
Bauke Scholtz <balusc@gmail.com>
2012-06-01 17:34:52 -0400

    some improvements

--- src/org/omnifaces/showcase/facesviews/PostRedirectBean.java
+++ src/org/omnifaces/showcase/facesviews/PostRedirectBean.java
@@ -6,13 +6,13 @@ import javax.faces.bean.ManagedBean;
 @RequestScoped
 @ManagedBean
 public class PostRedirectBean {
-    
-    public String toViewsDemo() {
-        return "/viewsdemo?faces-redirect=true";
-    }
 
-    public String toSecond() {
-        return "/other/second?faces-redirect=true";
-    }
-    
+	public String toViewsDemo() {
+		return "/viewsdemo?faces-redirect=true";
+	}
+
+	public String toSecond() {
+		return "/other/second?faces-redirect=true";
+	}
+
 }
