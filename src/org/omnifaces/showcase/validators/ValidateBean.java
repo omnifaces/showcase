eb4c48f3d029597a4b77eed7cd253ca6ee55ef3b
Arjan Tijms <arjan.tijms@gmail.com>
2013-06-10 20:46:20 +0200

    MyFaces requires view scoped beans to be Serializable.

--- src/org/omnifaces/showcase/validators/ValidateBean.java
+++ src/org/omnifaces/showcase/validators/ValidateBean.java
@@ -1,5 +1,7 @@
 package org.omnifaces.showcase.validators;
 
+import java.io.Serializable;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.validation.constraints.Pattern;
@@ -7,8 +9,10 @@ import javax.validation.constraints.Size;
 
 @ManagedBean
 @ViewScoped
-public class ValidateBean {
+public class ValidateBean implements Serializable {
 
+	private static final long serialVersionUID = 6654746339417292962L;
+	
 	@Pattern(regexp="^(?=\\s*\\S).*$", message = "Please enter value") // not empty
 	@Size(min = 6, groups = MyValidationGroup.class, message = "Please enter at least 6 characters")
 	private String value;
