d1eb40f23899511d1b81a52e4a5eb11433948290
Arjan Tijms <arjan.tijms@gmail.com>
2013-06-15 20:37:00 +0200

    Added conversation scope to FacesViews demo.

--- src/org/omnifaces/showcase/facesviews/PostRedirectBean.java
+++ src/org/omnifaces/showcase/facesviews/PostRedirectBean.java
@@ -1,18 +1,44 @@
 package org.omnifaces.showcase.facesviews;
 
-import javax.enterprise.context.RequestScoped;
-import javax.faces.bean.ManagedBean;
+import java.io.Serializable;
+import java.util.Date;
 
-@RequestScoped
-@ManagedBean
-public class PostRedirectBean {
+import javax.enterprise.context.Conversation;
+import javax.enterprise.context.ConversationScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+@Named
+@ConversationScoped
+public class PostRedirectBean implements Serializable {
+
+	private static final long serialVersionUID = -1209535053351028792L;
+	
+	private Date date = new Date();
+
+	@Inject
+	private Conversation conversation;
 
 	public String toViewsDemo() {
+		
+		if (conversation.isTransient()) {
+			conversation.begin();
+		}
+		
 		return "/viewsdemo?faces-redirect=true";
 	}
 
 	public String toSecond() {
+		
+		if (conversation.isTransient()) {
+			conversation.begin();
+		}
+
 		return "/other/second?faces-redirect=true";
+	}
+
+	public Date getDate() {
+		return date;
 	}
 
 }
