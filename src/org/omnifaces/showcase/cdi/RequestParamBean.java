d0820a7cf9bfa9c033bde091339d3a48b6a1d336
Arjan Tijms <arjan.tijms@gmail.com>
2013-07-10 00:29:31 +0200

    Latest snapshot and extended request parameter examples. WIP! Fully working link not adjusted yet, only failure case.

--- src/org/omnifaces/showcase/cdi/RequestParamBean.java
+++ src/org/omnifaces/showcase/cdi/RequestParamBean.java
@@ -10,6 +10,7 @@ import javax.faces.convert.DateTimeConverter;
 import javax.faces.validator.RequiredValidator;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.validation.constraints.NotNull;
 
 import org.omnifaces.cdi.requestparam.Attribute;
 import org.omnifaces.cdi.requestparam.Param;
@@ -19,15 +20,25 @@ import org.omnifaces.cdi.requestparam.ParamValue;
 @RequestScoped
 public class RequestParamBean {
 
+	// Custom validator with local validator message based on EL
 	@Inject @Param(validatorClasses = RequiredValidator.class, validatorMessage = "#{validatorMessages.requiredMsg}")
 	private ParamValue<String> text;
+	
+	// No explicit converters or validators, only a required attribute depending on the default message
+	@Inject	@Param(required = true)
+	private ParamValue<String> moretext;
 
+	// A very bare injection, but depending implicitly on the JSF default String to Integer converter
 	@Inject @Param
 	private ParamValue<Integer> number;
 
+	// A custom validator with a local attribute and message
 	@Inject @Param(converterClass = DateTimeConverter.class, converterAttributes = @Attribute(name="pattern", value="yyyyMMdd"), 
 				   converterMessage="{1}: \"{0}\" is not the date format we had in mind! Please use yyyyMMdd.")
 	private ParamValue<Date> date;
+	
+	@Inject @Param @NotNull
+	private ParamValue<String> foo;
 
 	private String result;
 
