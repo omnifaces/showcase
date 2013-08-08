102cf05d8da72134e9012fc18d2da263e5a0c6b2
Bauke Scholtz <balusc@gmail.com>
2013-08-08 17:34:05 -0400

    fixed/improved CDI @Param showcase

--- src/org/omnifaces/showcase/cdi/RequestParamBean.java
+++ src/org/omnifaces/showcase/cdi/RequestParamBean.java
@@ -7,7 +7,7 @@ import java.util.Date;
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
 import javax.faces.convert.DateTimeConverter;
-import javax.faces.validator.RequiredValidator;
+import javax.faces.validator.LengthValidator;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.validation.constraints.NotNull;
@@ -20,25 +20,30 @@ import org.omnifaces.cdi.requestparam.ParamValue;
 @RequestScoped
 public class RequestParamBean {
 
-	// Custom validator with local validator message based on EL
-	@Inject @Param(validatorClasses = RequiredValidator.class, validatorMessage = "#{validatorMessages.requiredMsg}")
-	private ParamValue<String> text;
-	
-	// No explicit converters or validators, only a required attribute depending on the default message
+	// Like <f:viewParam name="text1" value="#{bean.text1}" required="true">
 	@Inject	@Param(required = true)
-	private ParamValue<String> moretext;
+	private ParamValue<String> text1;
 
-	// A very bare injection, but depending implicitly on the JSF default String to Integer converter
+	// Like <f:viewParam name="text2" value="#{bean.text2}" validatorMessage="..."><f:validateLength minimum="3">
+	@Inject @Param(validatorClasses = LengthValidator.class,
+		validatorAttributes = @Attribute(name="minimum", value="3"),
+		validatorMessage = "{1}: Value is too too small! Please enter a minimum of 3 characters.")
+	private ParamValue<String> text2;
+
+	// Like <f:viewParam name="text1" value="#{bean.text1}"> using JSR303 bean validation.
+	@Inject @Param @NotNull
+	private ParamValue<String> text3;
+
+	// Like <f:viewParam name="number" value="#{bean.number}"> using implicit JSF integer converter.
 	@Inject @Param
 	private ParamValue<Integer> number;
 
-	// A custom validator with a local attribute and message
-	@Inject @Param(converterClass = DateTimeConverter.class, converterAttributes = @Attribute(name="pattern", value="yyyyMMdd"), 
-				   converterMessage="{1}: \"{0}\" is not the date format we had in mind! Please use yyyyMMdd.")
+	// Like <f:viewParam name="date" value="#{bean.date}" converterMessage="..."><f:convertDateTime pattern="yyyyMMdd">
+	@Inject @Param(
+		converterClass = DateTimeConverter.class,
+		converterAttributes = @Attribute(name="pattern", value="yyyyMMdd"),
+		converterMessage="{1}: \"{0}\" is not the date format we had in mind! Please use yyyyMMdd format.")
 	private ParamValue<Date> date;
-	
-	@Inject @Param @NotNull
-	private ParamValue<String> foo;
 
 	private String result;
 
@@ -49,13 +54,15 @@ public class RequestParamBean {
 			return;
 		}
 
-		String text = this.text.getValue();
+		String text1 = this.text1.getValue();
+		String text2 = this.text2.getValue();
+		String text3 = this.text3.getValue();
 		Integer number = this.number.getValue();
 		Date date = this.date.getValue();
 
 		result = String.format(
-			"You entered text '%s', number '%d' and date '%3$tY%3$tm%3$td'.",
-			text, number, date);
+			"You entered text1 '%s', text2 '%s', text3 '%s', number '%d' and date '%5$tY%5$tm%5$td'.",
+			text1, text2, text3, number, date);
 	}
 
 	public String getResult() {
