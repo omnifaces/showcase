6a6efbddde978c1c5158a462b5b9420b3ec7a289
Bauke Scholtz <balusc@gmail.com>
2013-08-15 22:33:29 -0400

    update to 20130815

--- src/org/omnifaces/showcase/functions/ConvertersBean.java
+++ src/org/omnifaces/showcase/functions/ConvertersBean.java
@@ -1,8 +1,10 @@
 package org.omnifaces.showcase.functions;
 
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -17,6 +19,8 @@ public class ConvertersBean {
 	private String[] array;
 	private Set<String> set;
 	private Map<String, String> map;
+	private String[] bigArray;
+	private List<String> bigList;
 
 	@PostConstruct
 	public void init() {
@@ -31,6 +35,9 @@ public class ConvertersBean {
 		map.put("key1", "value1");
 		map.put("key2", "value2");
 		map.put("key3", "value3");
+
+		bigArray = new String[] { "item1", "item2", "item3", "item4", "item5", "item6", "item7", "item8", "item9" };
+		bigList = Arrays.asList(bigArray);
 	}
 
 	public String[] getArray() {
@@ -44,7 +51,15 @@ public class ConvertersBean {
 	public Map<String, String> getMap() {
 		return map;
 	}
-	
+
+	public String[] getBigArray() {
+		return bigArray;
+	}
+
+	public List<String> getBigList() {
+		return bigList;
+	}
+
 	public Iterable<Integer> getIterable() {
 		return new TestIterable();
 	}
@@ -76,5 +91,5 @@ public class ConvertersBean {
 		}
 
 	}
-	
+
 }
