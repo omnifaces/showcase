4d2f0a62c252053eda0e559583d22d9e9de8d7a0
Bauke Scholtz <bauke.scholtz@kizitos.com>
2014-08-29 16:07:19 +0200

    Improved graphicImage showcase to show <ui:repeat><o:graphicImage> possibility.

--- src/main/java/org/omnifaces/showcase/components/GraphicImageBean.java
+++ src/main/java/org/omnifaces/showcase/components/GraphicImageBean.java
@@ -22,7 +22,13 @@ public class GraphicImageBean {
 	public byte[] getContent(Long id) throws IOException {
 		// Note: this is a dummy example. In reality, you should be able to return the desired byte[] content from some
 		// service class by given ID.
-		return Utils.toByteArray(getLogo());
+		return Utils.toByteArray(Faces.getResourceAsStream("/resources/layout/img/logo-70x70-" + id + ".png"));
+	}
+
+	public Long[] getIds() {
+		// Note: this is just a dummy example. In reality, you should be able to obtain them from another request/view
+		// scoped bean as ID of an entity representing the image.
+		return new Long[] { 1L, 2L, 3L, 4L, 5L, 6L, 7L };
 	}
 
 }
