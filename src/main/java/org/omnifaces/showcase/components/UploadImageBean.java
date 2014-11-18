97987a546112904c9f18e846d753381687ce77dd
Bauke Scholtz <bauke.scholtz@kizitos.com>
2014-11-18 14:37:07 +0100

    Add JSF 2.2 file upload example with data URI preview to graphicImage showcase

---
+++ src/main/java/org/omnifaces/showcase/components/UploadImageBean.java
@@ -0,0 +1,34 @@
+package org.omnifaces.showcase.components;
+
+import java.io.IOException;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.servlet.http.Part;
+
+import org.omnifaces.util.Utils;
+
+@ManagedBean
+@RequestScoped // Can be @ViewScoped, but caution should be taken with byte[] property. You don't want to save it in session.
+public class UploadImageBean {
+
+	private Part file;
+	private byte[] content;
+
+	public void upload() throws IOException {
+		content = Utils.toByteArray(file.getInputStream());
+	}
+
+	public Part getFile() {
+		return file;
+	}
+
+	public void setFile(Part file) {
+		this.file = file;
+	}
+
+	public byte[] getContent() {
+		return content;
+	}
+
+}
