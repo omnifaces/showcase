a22f8fa240b97200fb986872969c837d56579eeb
arjan.tijms <arjan.tijms@kizitos.com>
2014-11-19 18:19:29 +0100

    ListIndexConverter example now uses the full original list. Binding to the PF model source didn't work, as that one changes between selections and the converter is used for both selected as well as non-selected items.

--- src/main/java/org/omnifaces/showcase/converters/SelectListBean.java
+++ src/main/java/org/omnifaces/showcase/converters/SelectListBean.java
@@ -4,6 +4,7 @@ import static java.util.Arrays.asList;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
@@ -18,18 +19,26 @@ public class SelectListBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	private List<ExampleEntity> fullList;
 	private DualListModel<ExampleEntity> model;
 
 	@PostConstruct
 	public void init() {
+		
+		fullList = asList(
+			new ExampleEntity(1l, "Amsterdam"),
+			new ExampleEntity(2l, "Frankfurt"),
+			new ExampleEntity(3l, "Berlin")
+		);
+		
 		model = new DualListModel<>(
-			asList(
-				new ExampleEntity(1l, "Amsterdam"),
-				new ExampleEntity(2l, "Frankfurt"),
-				new ExampleEntity(3l, "Berlin")
-			),
+			new ArrayList<>(fullList),
 			new ArrayList<ExampleEntity>()
 		);
+	}
+	
+	public List<ExampleEntity> getFullList() {
+		return fullList;
 	}
 
 	public DualListModel<ExampleEntity> getModel() {
