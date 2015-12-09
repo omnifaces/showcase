523df8f46ca4654fcbd0da0474218195b6ebf4c7
Bauke Scholtz <bauke.scholtz@kizitos.com>
2015-12-09 22:56:30 +0100

    Updated whatsnew for 2.3 and added o:socket showcase

---
+++ src/main/java/org/omnifaces/showcase/push/PushBean.java
@@ -0,0 +1,40 @@
+package org.omnifaces.showcase.push;
+
+import java.io.Serializable;
+import java.util.concurrent.atomic.AtomicLong;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.omnifaces.cdi.ViewScoped;
+import org.omnifaces.cdi.push.PushContext;
+
+@Named
+@ViewScoped
+public class PushBean implements Serializable {
+
+	private static AtomicLong counter = new AtomicLong();
+
+	private boolean enabled;
+
+	@Inject
+	private PushContext push;
+
+	public void toggle() {
+		enabled = !enabled;
+	}
+
+	public void increment() {
+		long newvalue = counter.incrementAndGet();
+		push.send("counter", newvalue);
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public Long getCounter() {
+		return counter.get();
+	}
+
+}
