888a77b854dfb48af4afdf0bcd95afba89ecdd33
Bauke Scholtz <bauke.scholtz@kizitos.com>
2016-06-09 16:18:12 +0200

    Update to PF6. Updated whatsnew. Added p:autoComplete to ListConverter

---
+++ src/main/java/org/omnifaces/showcase/converters/AutoCompleteBean.java
@@ -0,0 +1,43 @@
+package org.omnifaces.showcase.converters;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Named;
+
+import org.omnifaces.cdi.ViewScoped;
+import org.omnifaces.showcase.model.ExampleEntity;
+
+@Named
+@ViewScoped
+public class AutoCompleteBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private ExampleEntity selectedEntity;
+	private List<ExampleEntity> availableEntities;
+
+	public List<ExampleEntity> autoComplete(String query) {
+		availableEntities = new ArrayList<>();
+
+		for (long i = 0; i < 5; i++) {
+			availableEntities.add(new ExampleEntity(i, query + i));
+		}
+
+		return availableEntities;
+	}
+
+	public ExampleEntity getSelectedEntity() {
+		return selectedEntity;
+	}
+
+	public void setSelectedEntity(ExampleEntity selectedEntity) {
+		this.selectedEntity = selectedEntity;
+	}
+
+	public List<ExampleEntity> getAvailableEntities() {
+		return availableEntities;
+	}
+
+}
