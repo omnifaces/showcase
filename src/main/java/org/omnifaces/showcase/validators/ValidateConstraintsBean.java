3b19983dfb56fff0a84bd158cd354d3bd3b24dbd
Bauke Scholtz <balusc@gmail.com>
2022-11-30 14:40:08 -0400

    Fix #18, explicit scope is requried

--- src/main/java/org/omnifaces/showcase/validators/ValidateConstraintsBean.java
+++ src/main/java/org/omnifaces/showcase/validators/ValidateConstraintsBean.java
@@ -1,10 +1,12 @@
 package org.omnifaces.showcase.validators;
 
+import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Named;
 
 import org.omnifaces.showcase.model.ConstrainedEntity;
 
 @Named
+@RequestScoped
 public class ValidateConstraintsBean {
 
 	private ConstrainedEntity constrainedEntity = new ConstrainedEntity();
