191212bf73bb7d4396cf13f76908a21c8e4d8d56
Bauke Scholtz <balusc@gmail.com>
2024-05-11 09:47:38 -0400

    remove unused properties

--- src/main/java/org/omnifaces/showcase/converters/GenericEnumBean.java
+++ src/main/java/org/omnifaces/showcase/converters/GenericEnumBean.java
@@ -1,7 +1,6 @@
 package org.omnifaces.showcase.converters;
 
 import java.io.Serializable;
-import java.util.List;
 
 import jakarta.inject.Named;
 
@@ -15,8 +14,6 @@ public class GenericEnumBean implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private ExampleEnum[] enumArray;
-	private List<ExampleEnum> enumList;
-	private List<ExampleEnum> enumListConverted;
 
 	public ExampleEnum[] getEnumArray() {
 		return enumArray;
@@ -24,22 +21,6 @@ public class GenericEnumBean implements Serializable {
 
 	public void setEnumArray(ExampleEnum[] enumArray) {
 		this.enumArray = enumArray;
-	}
-
-	public List<ExampleEnum> getEnumList() {
-		return enumList;
-	}
-
-	public void setEnumList(List<ExampleEnum> enumList) {
-		this.enumList = enumList;
-	}
-
-	public List<ExampleEnum> getEnumListConverted() {
-		return enumListConverted;
-	}
-
-	public void setEnumListConverted(List<ExampleEnum> enumListConverted) {
-		this.enumListConverted = enumListConverted;
 	}
 
 }
