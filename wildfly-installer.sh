266ad21e900094e9bf253bb3314f2487cfad652d
Bauke Scholtz <balusc@gmail.com>
2024-08-25 09:00:28 -0400

    bump WildFly version

--- wildfly-installer.sh
+++ wildfly-installer.sh
@@ -10,8 +10,8 @@
 #tested-version2 :10.0.0.Final
 #tested-distros2 :Debian 8
 
-WILDFLY_NAME=wildfly32
-WILDFLY_VERSION=32.0.0.Final
+WILDFLY_NAME=wildfly33
+WILDFLY_VERSION=33.0.1.Final
 WILDFLY_FILENAME=wildfly-$WILDFLY_VERSION
 WILDFLY_ARCHIVE_NAME=$WILDFLY_FILENAME.tar.gz
 WILDFLY_DOWNLOAD_ADDRESS=https://github.com/wildfly/wildfly/releases/download/$WILDFLY_VERSION/$WILDFLY_ARCHIVE_NAME
